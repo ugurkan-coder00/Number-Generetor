5898388f646c52f681d08cb98cac397c692f5445
Uğurkan <83974614+ugurkan-coder00@users.noreply.github.com>
2021-11-07 18:50:40 +0300

    Update random.h

--- random.h
+++ random.h
@@ -7,9 +7,17 @@
 
     class Random{
         public:
-            int randint(int how_many /* how many : 100 = 0 - 99, 101 = 1 - 100, 30 - 1985 = 1985 - 2014 */){
-                srand(time(0));
-                for (int i = 0; i < 4; i++){
+            int randint(int how_many,bool loop){
+                // how many : 100 = 0 - 99, 101 = 1 - 100, 30 - 1985 = 1985 - 2014
+                // loop = true :  The program generates a different number each time it compiles
+                // loop = false : generates a number only once
+                if (loop == true){
+                    srand(time(0));
+                    for (int i = 0; i < 4; i++){
+                        return rand() % how_many;
+                    }
+                }
+                else if (loop == false){
                     return rand() % how_many;
                 }
             }
